c53366b6e4be9c5b96d0a506c5fd0829086cb51f
Joao <joao@joao-R590.(none)>
2011-10-24 00:21:50 -0200

    tentando adaptar header do refinery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def replace_css lista
+    lista.collect do |e|
+      e = "first-child" if e == "first"
+      e = "last-child" if e == "last"
+      e = "current" if e == "selected"
+      e
+    end
+  end
+
 end
