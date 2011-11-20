cfb596c1da892d913f4f5edb267d73bcb48f0a47
Marcelle <mguine@gmail.com>
2011-11-20 21:22:24 -0200

    Feito layout calendario e alguns detalhes de home

--- vendor/engines/events/app/models/event.rb
+++ vendor/engines/events/app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ActiveRecord::Base
+
   has_event_calendar
   acts_as_indexed :fields => [:name]
+
+	def color
+	  '#2670C3'
+	end
 
   validates :name, :presence => true, :uniqueness => true
   
