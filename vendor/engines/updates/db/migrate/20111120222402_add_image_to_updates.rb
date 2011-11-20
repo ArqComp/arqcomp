eed59ecb4edaea63edae6583d475d9cefe901666
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2011-11-20 21:15:34 -0200

    adicionar imagem a noticias

--- vendor/engines/updates/db/migrate/20111120222402_add_image_to_updates.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToUpdates < ActiveRecord::Migration
-  def self.up
-    add_column :updates, :image_id, :integer
-  end
-
-  def self.down
-    remove_column :updates, :image_id
-  end
-end
