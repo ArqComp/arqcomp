9451c82608fc477e060f57dc75f8d698271ec558
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2011-11-22 01:43:34 -0200

    ajustes em noticias

--- vendor/engines/updates/app/models/update.rb
+++ vendor/engines/updates/app/models/update.rb
@@ -2,7 +2,7 @@ class Update < ActiveRecord::Base
 
   acts_as_indexed :fields => [:title, :text]
 
-  validates :title, :presence => true, :uniqueness => true
+  validates :title, :presence => true, :uniqueness => true, :length => { :maximum => 32 }
   
   belongs_to :image, :class_name => 'Image'
   
