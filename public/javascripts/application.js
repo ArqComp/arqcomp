73dab4c0583b6f968ff3dff65ac740ea0bef1adf
João Felipe N. Pimentel <joaofelipenp@gmail.com>
2011-11-24 11:37:27 -0200

    page call mudando

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    $(".page-call").html($("#page-call-default").html());
+    $(".page-call-item").hover(function(){
+       $(".page-call").html($("#page-call-"+this.id).html());
+    });
+    $(".page-call-item").mouseleave(function(){
+       $(".page-call").html($("#page-call-default").html());
+    });
+    
     $(".project-content").hide();
     $(".project").click(function(){
         $("#"+this.id+"-content").toggle();
